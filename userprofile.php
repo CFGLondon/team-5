d13b6ece4a3c7ff1a23afeaaae15e89b25267c55
Andrew Ramsier <andrewramsier@gmail.com>
2015-12-05 05:38:57 +0000

    added a table to userprofile

--- userprofile.php
+++ userprofile.php
@@ -83,6 +83,41 @@ function get_age($birth_date){
 							<!-- Description -->
 							<p>My Qualifications go here please</p>
 							<br></br>
+							<table>
+<tr>
+	<td>
+		<div name="textNo" value="1">1</div>
+	</td>
+
+			
+			
+	<td>
+		<select name="QualList">
+			<option value="">Qualification Type</option>
+			<option value="gcse">GCSE</option>
+			<option value="btec">BTEC</option>
+			<option value="alevel">ALEVEL</option>
+			<option value="hons">HONS</option>
+			<option value="masters">MASTERS</option>
+		</select>
+	</td>
+	<td>
+		<select name="QualDisc">
+			<option value="">Qualification Discipline</option>
+			<option value="Mathematics">Mathematics</option>
+			<option value="English">English</option>
+			<option value="Science">Science</option>
+			<option value="ICT">ICT</option>
+		</select>
+	</td>
+	<td>
+		<input type="submit" name="sub"/>
+	</td>
+			
+	
+	
+</tr>
+</table>
 							<p> Give me skills</p>
 							<div class="social">
 								<a href="https://www.facebook.com/andrew.ramsier" 
